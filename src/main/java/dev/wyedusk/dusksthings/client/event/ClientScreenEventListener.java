d1439d9db62843a124f6ae780a2ddc8b5a39a364
Dusk <wyedusk+github@gmail.com>
2026-09-13 13:08:11 +0100

    loadout buttons on inventory menu

---
+++ src/main/java/dev/wyedusk/dusksthings/client/event/ClientScreenEventListener.java
@@ -0,0 +1,23 @@
+package dev.wyedusk.dusksthings.client.event;
+
+import dev.wyedusk.dusksthings.client.rendering.gui.widget.LoadoutButton;
+import dev.wyedusk.dusksthings.common.DusksThings;
+import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ScreenEvent;
+
+@EventBusSubscriber(modid = DusksThings.MODID, value = Dist.CLIENT)
+public class ClientScreenEventListener {
+    @SubscribeEvent
+    public static void onScreenInit(ScreenEvent.Init.Post event) {
+        if (event.getScreen() instanceof InventoryScreen inventory) {
+            int x = inventory.getGuiLeft() + inventory.getXSize();
+            int y = inventory.getGuiTop();
+            event.addListener(new LoadoutButton(x + 5, y + 5, 1));
+            event.addListener(new LoadoutButton(x + 5, y + 30, 2));
+            event.addListener(new LoadoutButton( x + 5, y + 55, 3));
+        }
+    }
+}
---
+++ src/main/java/dev/wyedusk/dusksthings/client/rendering/gui/widget/LoadoutButton.java
@@ -0,0 +1,51 @@
+package dev.wyedusk.dusksthings.client.rendering.gui.widget;
+
+import dev.wyedusk.dusksthings.common.DusksThings;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+public class LoadoutButton extends AbstractWidget {
+    private final ResourceLocation texture;
+    private final ResourceLocation highlightTexture;
+    private final ResourceLocation disabledTexture;
+    private final int loadoutNumber;
+
+    public LoadoutButton(int x, int y, int loadoutNumber) {
+        super(x, y, 20, 20, Component.empty());
+        String loadoutNumberText = loadoutNumber == 1 ? "one" : loadoutNumber == 2 ? "two" : "three";
+        this.texture = ResourceLocation.fromNamespaceAndPath(DusksThings.MODID, "textures/gui/sprites/widget/loadout_%s.png".formatted(loadoutNumberText));
+        this.highlightTexture = ResourceLocation.fromNamespaceAndPath(DusksThings.MODID, "textures/gui/sprites/widget/loadout_%s_highlighted.png".formatted(loadoutNumberText));
+        this.disabledTexture = ResourceLocation.fromNamespaceAndPath(DusksThings.MODID, "textures/gui/sprites/widget/loadout_%s_disabled.png".formatted(loadoutNumberText));
+        this.loadoutNumber = loadoutNumber;
+    }
+
+    @Override
+    protected void renderWidget(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        ResourceLocation currentTexture;
+
+        if (!this.active) {
+            currentTexture = disabledTexture;
+        } else if (this.isHoveredOrFocused()) {
+            currentTexture = highlightTexture;
+        } else {
+            currentTexture = texture;
+        }
+
+        guiGraphics.blit(currentTexture, this.getX(), this.getY(), 0, 0, 20, 20, 20, 20);
+    }
+
+    @Override
+    public void onClick(double mouseX, double mouseY) {
+        if (this.active) {
+            Minecraft.getInstance().player.sendSystemMessage(Component.literal("loadout %s".formatted(this.loadoutNumber)));
+        }
+    }
+
+    @Override
+    protected void updateWidgetNarration(@NotNull NarrationElementOutput narrationElementOutput) {}
+}
